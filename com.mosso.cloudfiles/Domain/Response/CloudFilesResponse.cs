2db0f89c90073eb6a8989f10feb84d4ac811deb3
unknown <User@.(none)>
2010-07-22 16:42:52 +1200

    Converted project to Visual Studio 2010 (Visual C# Express: http://www.microsoft.com/express/downloads/).targeting .NET Framework 4.0 (default).
    Modified HasTextBody() in CloudFilesResponse.cs which GetContainerItemList to fail to return Objects (due to ContentType being returned as "text/plain; charset=utf8" rather than UTF-8 or utf-8.

--- com.mosso.cloudfiles/Domain/Response/CloudFilesResponse.cs
+++ com.mosso.cloudfiles/Domain/Response/CloudFilesResponse.cs
@@ -42,11 +42,15 @@ namespace com.mosso.cloudfiles.domain.response
 
         private bool HasTextBody()
         {
-            return (_webResponse.ContentType.Contains("application/json") ||
-                _webResponse.ContentType=="application/xml"||
-                    _webResponse.ContentType == "application/xml; charset=utf-8" ||
-                    _webResponse.ContentType.Contains("text/plain") && _webResponse.ContentLength == -1) ||
-                   _webResponse.ContentType == "text/plain; charset=UTF-8";
+            return (
+                _webResponse.ContentType.Contains("application/json") ||
+                _webResponse.ContentType == "application/xml" ||
+                _webResponse.ContentType == "application/xml; charset=utf-8" ||
+                _webResponse.ContentType.Contains("text/plain") && _webResponse.ContentLength == -1) ||
+                //_webResponse.ContentType == "text/plain; charset=UTF-8" // asw: replaced
+                _webResponse.ContentType.Contains("text/plain")
+                ;
+
         }
 
         private void CopyToMemory(Stream input, Stream output)
